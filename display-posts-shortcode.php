e73918c0a72f2bac3e29f06413def6c9cd7ddf9a
John Law <chihonlaw@gmail.com>
2018-03-10 01:54:06 -0800

    Split the content and used read-more short code

--- display-posts-shortcode.php
+++ display-posts-shortcode.php
@@ -470,8 +470,15 @@ function be_display_posts_shortcode( $atts ) {
 
 		if( $include_content ) {
 			add_filter( 'shortcode_atts_display-posts', 'be_display_posts_off', 10, 3 );
+
 			/** This filter is documented in wp-includes/post-template.php */
-			$content = '<div class="' . implode( ' ', $content_class ) . '">' . apply_filters( 'the_content', get_the_content() ) . '</div>';
+			$explodedArray = get_the_content();
+
+			$getContent = explode("\n" ,$explodedArray[0], 2);
+			$showContent .= $getContent[0] . "[read more='Read more' less='Read less']" . $getContent[1] . "[/read]";
+
+			/** This filter is documented in wp-includes/post-template.php */
+			$content = '<div class="' . implode( ' ', $content_class ) . '">' . apply_filters( 'the_content', $showContent ) . '</div>';
 			remove_filter( 'shortcode_atts_display-posts', 'be_display_posts_off', 10, 3 );
 		}
 
